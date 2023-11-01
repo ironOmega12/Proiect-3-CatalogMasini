4ba96c7ff8b074c88f144a91e2082c6754f9acd7
vasile meghesan <mvasy03@yahoo.ro>
2023-11-01 03:51:35 -0700

    creat loggin page folodin singletone si stocare informatiilor intr un fisier credentials.txt care va fi creat automat in momentul logginu lui

--- interfata/CatalogMasiniUI.java
+++ interfata/CatalogMasiniUI.java
@@ -1,42 +1,104 @@
 package interfata;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
-import javax.swing.*;
+import vehicule.LoggerSingleton;
 
 public class CatalogMasiniUI {
-    private JFrame frameStart;
-    private ImageIcon carBackground;
-    private JLabel mainStartBg;
+    private JFrame frameLogin;
+    private JLabel mainLoginBg;
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private LoggerSingleton logger = LoggerSingleton.getInstance();
+    private static final String CREDENTIALS_FILE = "credentials.txt";
 
     public CatalogMasiniUI() {
-        
-        carBackground = new ImageIcon(this.getClass().getResource("backgroundMain.jpg"));
-        mainStartBg = new JLabel(carBackground);
-        mainStartBg.setSize(500, 600);
 
-        frameStart = new JFrame("Pagina Start");
-        frameStart.setSize(500, 600);
-        frameStart.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frameStart.setResizable(false);
-        frameStart.setLocationRelativeTo(null);  // This will center the frame.
+        frameLogin = new JFrame("Login Page");
+        frameLogin.setSize(500, 600);
+        frameLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameLogin.setResizable(false);
+        frameLogin.setLocationRelativeTo(null);
 
 
-        TransparentRoundedButton butonStart = new TransparentRoundedButton("Start");
-        butonStart.setBounds(190, 380, 100, 50);
+        mainLoginBg = new JLabel(new ImageIcon(this.getClass().getResource("backgroundMain.jpg")));
+        mainLoginBg.setSize(500, 600);
 
 
-        mainStartBg.add(butonStart);
-        frameStart.add(mainStartBg);
+        usernameField = new JTextField();
+        usernameField.setBounds(190, 240, 100, 30);
 
-        butonStart.addActionListener(new ActionListener() {
+        passwordField = new JPasswordField();
+        passwordField.setBounds(190, 280, 100, 30);
+
+        TransparentRoundedButton loginButton = new TransparentRoundedButton("Login");
+        loginButton.setBounds(190, 320, 100, 50);
+
+        mainLoginBg.add(usernameField);
+        mainLoginBg.add(passwordField);
+        mainLoginBg.add(loginButton);
+        frameLogin.add(mainLoginBg);
+
+        loginButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                frameStart.dispose();
-                new CatalogMasiniStart();
+                String username = usernameField.getText();
+                String password = new String(passwordField.getPassword());
+
+                if (isValidLogin(username, password)) {
+                    frameLogin.dispose();
+                    new CatalogMasiniStart();
+
+
+                    logger.log("Login successful for user: " + username);
+                } else {
+                    JOptionPane.showMessageDialog(frameLogin, "Invalid login credentials", "Login Failed", JOptionPane.ERROR_MESSAGE);
+                }
             }
         });
 
-        frameStart.setVisible(true);
+        frameLogin.setVisible(true);
+    }
+
+    private boolean isValidLogin(String username, String password) {
+        if (username.isEmpty() || password.isEmpty()) {
+            JOptionPane.showMessageDialog(frameLogin, "Username and password must not be empty", "Login Failed", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        if (!username.matches(".*\\d{3,}.*")) {
+            JOptionPane.showMessageDialog(frameLogin, "Username must contain at least 3 digits", "Login Failed", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        if (!username.matches("^[a-zA-Z0-9]*$")) {
+            JOptionPane.showMessageDialog(frameLogin, "Username must contain only letters and digits", "Login Failed", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        if (!password.matches("^\\d+$")) {
+            JOptionPane.showMessageDialog(frameLogin, "Password must contain only digits", "Login Failed", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        return true;
+    }
+
+    private void storeLoginInfo(String username, String password) {
+        try (FileWriter writer = new FileWriter(CREDENTIALS_FILE, true);
+             BufferedWriter bufferedWriter = new BufferedWriter(writer)) {
+            bufferedWriter.write(username + ":" + password);
+            bufferedWriter.newLine();
+        } catch (IOException e){}
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new CatalogMasiniUI();
+        });
     }
 }
